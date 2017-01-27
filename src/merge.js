8ab57cfca4fa3da730c860447c1314ad18c8c093
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2017-01-28 00:20:37 +0100

    0.5.2: fix row values parsing when using expressions with whitespaces

--- src/merge.js
+++ src/merge.js
@@ -1,5 +1,5 @@
 function mergeDecls({ rowDecl, columnDecl, areaDecl }) {
-	let rowValues = rowDecl.value.split(/\s+/);
+	let rowValues = rowDecl.value.split(/\s+(?![^(]*\))/);
 	let areasRows = areaDecl.value.match(/"[^"]*"/g);
 	let rows = areasRows.map((areasRow, i) => `${areasRow} ${rowValues[i] || ''}`);
 	return `${rows.join(' ')} / ${columnDecl.value}`;
